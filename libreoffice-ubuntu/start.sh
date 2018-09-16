0ac0185b47663a4f56978a7329ac0339d3eef732
bambooG130 <i.321.takeji@gmail.com>
2018-09-16 23:00:31 +0900

    Modified: add fcitx-autostart to host

--- libreoffice-ubuntu/start.sh
+++ libreoffice-ubuntu/start.sh
@@ -1,6 +1,7 @@
 #/bin/bash -
 
-docker run --rm -i -t --net host -e DISPLAY=$DISPLAY \
-                                 -v $HOME/.Xauthority:/root/.Xauthority:rw \
-                                 libreoffice:ubuntu-16.04 "$@"
+bash -c 'fcitx-autostart &&
+         docker run --rm -i -t --net host -e DISPLAY=$DISPLAY \
+                                          -v $HOME/.Xauthority:/root/.Xauthority:rw \
+                                          libreoffice:ubuntu-16.04 "$@"'
                                  #-v $HOME:/work-dir \
