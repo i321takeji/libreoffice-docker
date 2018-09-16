b2a8049db3aae89b7d8ffff9b7a856ca26c94300
bambooG130 <i.321.takeji@gmail.com>
2018-09-16 19:31:45 +0900

    Modified: add m+ fonts

--- libreoffice-ubuntu/start.sh
+++ libreoffice-ubuntu/start.sh
@@ -2,5 +2,5 @@
 
 docker run --rm -i -t --net host -e DISPLAY=$DISPLAY \
                                  -v $HOME/.Xauthority:/root/.Xauthority:rw \
-                                 -v $HOME:/work-dir \
                                  libreoffice:ubuntu-16.04 "$@"
+                                 #-v $HOME:/work-dir \
