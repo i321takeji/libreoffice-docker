fc5dbf63f32ec02e125a7389b4fc575ebf3c7549
bambooG130 <i.321.takeji@gmail.com>
2018-09-17 12:28:21 +0900

    Modified: start script

--- libreoffice-ubuntu/start.sh
+++ libreoffice-ubuntu/start.sh
@@ -1,7 +1,22 @@
 #/bin/bash -
 
-bash -c 'fcitx-autostart &&
+set -e
+
+WORK_DIR="$1"
+WORK_VOLUME=""
+
+if [ -n "${WORK_DIR}" ]; then
+  if [ -d "${WORK_DIR}" ]; then
+    WORK_ABSPATH="$(cd ${WORK_DIR} && pwd)"
+    WORK_VOLUME="-v ${WORK_ABSPATH}:/work-dir"
+  else
+    echo "${WORK_DIR} is NOT directory."
+    exit 1
+  fi
+fi
+
+bash -c "fcitx-autostart &&
          docker run --rm -i -t --net host -e DISPLAY=$DISPLAY \
                                           -v $HOME/.Xauthority:/root/.Xauthority:rw \
-                                          libreoffice:ubuntu-16.04 "$@"'
-                                 #-v $HOME:/work-dir \
+                                          ${WORK_VOLUME} \
+                                          libreoffice:ubuntu-16.04 $@"
